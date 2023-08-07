af1c406d75f4da35c40aeb458f9909cc4e0199ef
Andriamah <80816142+Andriamah@users.noreply.github.com>
2023-08-08 00:23:07 +0100

    app.disable('etag');

--- server.js
+++ server.js
@@ -9,6 +9,7 @@ app.use(express.json());
 app.use(bodyParser.urlencoded({ extended: true }))
 // parse requests of content-type - application/json
 app.use(bodyParser.json())
+app.disable('etag');
 // define a root route
 app.get('/', (req, res) => {
   res.send("Hello World");
